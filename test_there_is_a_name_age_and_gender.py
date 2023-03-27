d6305d091600faa67e6227293199f07c265c85ac
Elena-1032 <122686262+Elena-1032@users.noreply.github.com>
2023-03-27 23:41:09 +0300

    Add files via upload

---
+++ test_there_is_a_name_age_and_gender.py
@@ -0,0 +1,32 @@
+# 3 У всех питомцев есть имя, возраст и порода
+import pytest
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+def test_there_is_a_name_age_and_gender(go_to_my_pets):
+   element = WebDriverWait(pytest.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".table.table-hover tbody tr")))
+   pet_data = pytest.driver.find_elements(By.CSS_SELECTOR, '.table.table-hover tbody tr') # Сохраняем элементы с данными о питомцах
+   # Явное ожидание _______________________:
+   wait = WebDriverWait(pytest.driver, 10)
+   # Ожидаем, что данные всех питомцев, найденных локатором css_locator = '.table.table-hover tbody tr', видны на странице:
+   for i in range(len(pet_data)):
+      assert wait.until(EC.visibility_of(pet_data[i]))
+
+   # Ищем в теле таблицы все имена питомцев и ожидаем увидеть их на странице:
+   name_my_pets = pytest.driver.find_elements(By.XPATH, '//tbody/tr/td[1]')
+   for i in range(len(name_my_pets)):
+      assert wait.until(EC.visibility_of(name_my_pets[i]))
+
+   # Ищем в теле таблицы все породы питомцев и ожидаем увидеть их на странице:
+   type_my_pets = pytest.driver.find_elements(By.XPATH, '//tbody/tr/td[2]')
+   for i in range(len(type_my_pets)):
+      assert wait.until(EC.visibility_of(type_my_pets[i]))
+
+   # Ищем в теле таблицы все данные возраста питомцев и ожидаем увидеть их на странице:
+   age_my_pets = pytest.driver.find_elements(By.XPATH, '//tbody/tr/td[3]')
+   for i in range(len(age_my_pets)):
+      assert wait.until(EC.visibility_of(age_my_pets[i]))
+
+# Запускаем python -m pytest -v --driver Chrome --driver-path
+# /python/24_new/chromedriver.exe python/24_new/test_there_is_a_name_age_and_gender.py
